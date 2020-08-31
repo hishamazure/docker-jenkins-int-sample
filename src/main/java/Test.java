ed65bd984caf748afaa15cc90e607a5ec5f02c57
hisham <hisham@localhost>
2020-08-31 11:39:38 +0300

    Version 1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Again2");
+		System.out.println("Hello Again3");
 
 	}
 
